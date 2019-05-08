51c164c06d532a3d50b09404f206fbc76e4e7216
Matheus Fróes <matheusfroesmarques@gmail.com>
2019-05-08 12:39:49 -0300

    Initial Commit

---
+++ HoldRecordButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HoldRecordButton
+//
+//  Created by Matheus Fróes on 08/05/19.
+//  Copyright © 2019 Matheus Fróes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
